bf5015750b4b61312ff4b2d8740e1d0d541c36e6
PMKanyora <peterkanyora341@gmail.com>
2021-06-02 18:41:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/User.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+export const User = ({ name, email, id, onEdit, onDelete }) => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleEdit = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const handleDelete = () => {
+    onDelete(id);
+  };
+
+  const handleOnEditSubmit = (evt) => {
+    evt.preventDefault();
+    onEdit(id, evt.target.name.value, evt.target.email.value);
+    setIsEdit(!isEdit);
+  };
+
+  return (
+    <div>
+      {isEdit ? (
+        <div className="form">
+        <form onSubmit={handleOnEditSubmit} >
+          <input placeholder="Name" name="name" defaultValue={name} />
+          <input placeholder="Email" name="email" defaultValue={email} />
+          <button onSubmit={handleOnEditSubmit}>Save</button>
+        </form>
+        </div>
+      ) : (
+        <div className="user">
+          <span className="user-name">{name}</span>
+          <span className="user-email">{email}</span>
+          <div>
+            <button onClick={handleEdit} className="btn edit">
+              Edit
+            </button>
+            <button onClick={handleDelete} className="btn delete">
+              Delete
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
